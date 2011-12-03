994e4dd4a237c47241331de345eecd5bc651c137
Dave Johnson <dave.johnson@rollerweblogger.org>
2011-12-03 17:31:48 -0500

    Reorganized code into two modules instead of the nine or so we had before. I killed the assembly too. Want to keep things light and just have a WAR file. Also, got rid of documentation -- for now I'll just point to the Roller docs.

--- sign-jboss-release.sh
+++
@@ -1,5 +0,0 @@
-export rcstring="-RC5"
-export vstring="5.0.0"
-
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-jboss.tar.gz
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-jboss.zip
--- sign-tomcat-release.sh
+++
@@ -1,8 +0,0 @@
-export rcstring="-RC5"
-export vstring="5.0.0"
-
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-tomcat.tar.gz
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-for-tomcat.zip
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.tar.gz 
-gpg --armor --detach-sig weblogger-assembly/target/roller-weblogger-${vstring}${rcstring}-source.zip
-
